9c98f7bae8a380f92098624590d4b7239db07617
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-03-01 12:07:23 -0700

    Fixes lines 39 and 40 in about_hashes.rb.
    * expected = { :one => "eins", :two => "dos" }
     assert_equal expected, hash
    
    [#65851838]

--- koans/about_hashes.rb
+++ koans/about_hashes.rb
@@ -37,7 +37,7 @@ class AboutHashes < EdgeCase::Koan
     hash[:one] = "eins"
 
     expected = { :one => "eins", :two => "dos" }
-    #assert_equal {:one=>"eins", :two=>"dos"}, hash
+    assert_equal expected, hash
 
     # Bonus Question: Why was "expected" broken out into a variable
     # rather than used as a literal?
