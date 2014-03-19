2e26dbd0a28b42698ecab2986d0cf24d3b133efa
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-03-19 13:04:23 -0600

    Creates code to pass tests in about_triangle_project_2.rb

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,11 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+    raise TriangleError if a<=0 or b<=0 or c<=0
+    raise TriangleError if a+b<=c or b+c<=a or a+c<=b
+    return :equilateral if a==b and a==c
+    return :isosceles if a==b or b==c or a==c
+    :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
